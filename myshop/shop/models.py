7319124ff5197f0289fe9fa43cadb118a68dc8f9
hypnopompicindex <hypnopompicindex@gmail.com>
2016-11-11 01:37:52 -0500

    Added catalog views

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -13,6 +13,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products')
@@ -33,3 +36,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
