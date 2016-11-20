b800ad69d3aed862d83077dc1c542a7a7b1b99d4
hypnopompicindex <hypnopompicindex@gmail.com>
2016-11-20 01:39:04 -0500

    Added  synchronous task with celery

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -110,3 +110,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 from cart.cart import Cart
 
 
@@ -17,6 +18,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
